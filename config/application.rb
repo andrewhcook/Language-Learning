3ff3dc7062252a151b961eaed3278a5404b09ba2
andrewhcook <andrewhcook1@gmail.com>
2023-11-30 15:49:40 -0600

    removed require simple_form in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require 'simple_form'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
