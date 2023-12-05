dafde1c8349582a02f3f6cf46ab0206206df05bd
andrewhcook <andrewhcook1@gmail.com>
2023-12-05 10:01:16 -0600

    debugged tts

--- app/controllers/tts_controller.rb
+++ app/controllers/tts_controller.rb
@@ -6,7 +6,7 @@ class TtsController < ApplicationController
     openai_api_key = ENV['chat_gpt_key']
     openai = OpenAI::Client.new(access_token: openai_api_key)
 
-    audio_response = openai.audio.speech(parameters: { model: "tts-1-hd", voice: "alloy", input: text })
+    audio_response = openai.audio.speech(parameters: { model: "tts-1", voice: "alloy", input: text })
 
     # Send the binary audio data in the response
     send_data audio_response, type: 'audio/mp3', disposition: 'inline'
